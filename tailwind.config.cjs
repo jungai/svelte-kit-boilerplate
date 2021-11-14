efff27c505188810cb321968b70b5d765ee038c3
jungai <juworaphol125@gmail.com>
2021-11-14 14:56:17 +0700

    add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.{svelte,html}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
